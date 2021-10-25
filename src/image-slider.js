35928c43f774ef9983d395d6b0948f279271cbfc
Quo210 <gabrielrengifo211@gmail.com>
2021-10-25 00:19:03 -0400

    added next and prev image buttons

--- src/image-slider.js
+++ src/image-slider.js
@@ -12,22 +12,48 @@ const slides = (() => {
         }
     }
 
-    makeCatArray();
-
     function getImgArr(){
         return imgArray;
     }
 
+    function getNextSrc(current = '../images/cat1.jpeg'){
+        const arr = getImgArr();
+        let pos = arr.indexOf(current);
+
+        if (pos + 1 > arr.length - 1){
+            alert('Last image');
+            return false
+        }
+
+        return arr[pos + 1];
+    }
+
+    function getPrevSrc(current = '../images/cat8.jpeg'){
+        const arr = getImgArr();
+        let pos = arr.indexOf(current);
+
+        if (pos - 1 < 0){
+            alert('First Image');
+            return false
+        }
+
+        return arr[pos-1];
+    }
     
 
+    makeCatArray();
 
     return {
-        show: makeCatArray,
         getArr: getImgArr,
+        prevCat: getPrevSrc,
+        nextCat: getNextSrc,
     }
 })()
 
 const dom = (()=>{
+
+    let currentImage;
+
     function buildHolder(src = '../images/cat1.jpeg'){
         const imgHolder = document.createElement('div');
         imgHolder.classList.add('img-holder');
@@ -35,6 +61,7 @@ const dom = (()=>{
         img.classList.add('cat-img');
         img.setAttribute('src',`${src}`);
         imgHolder.appendChild(img);
+        currentImage = src;
         return imgHolder
     }
 
@@ -42,18 +69,83 @@ const dom = (()=>{
         return document.querySelector('section#frame')
     }
 
-    function placeHolder(elem){
+    function placeImageOnDoc(elem){
         const frame = getFrame();
         frame.appendChild(elem);
     }
 
+    function buildAndPlace(src){
+        const htmlElem = buildHolder(src);
+        placeImageOnDoc(htmlElem);
+    }
+
+    function currentCat(){
+        return currentImage;
+    }
+
+    function emptyFrame(){
+        let fra = getFrame();
+        fra.replaceChildren();
+    }
 
     return {
-        build: buildHolder,
-        placeImg: placeHolder,
+        showImg: buildAndPlace,
+        currentCat: currentCat,
+        empty: emptyFrame,
     }
 })();
 
+const counter = (()=>{
+    function fillCounter(arr){
+        const cou = getCounter(); // counter
+        for (let i = 0; i < arr.length; i++){
+            const thing = document.createElement('div');
+            thing.textContent = '+';
+            thing.classList.add('counter-thing');
+            thing.setAttribute('data-key',`${i+1}`)
+            cou.appendChild(thing);
+        }
+    }
+
+    function getCounter(){
+        return document.querySelector('section.counter')
+    }
+
+    return {
+        fill: fillCounter,
+    }
+
+})();
+
+
+
 const arr = slides.getArr()
-const my = dom.build(arr[6])
-dom.placeImg(my);
+const my = dom.showImg(arr[2])
+
+function nextCat(){
+    let cur = dom.currentCat(); // Current
+    let nex = slides.nextCat(cur); // Next
+    if (nex == false){
+        return
+    }
+    dom.empty();
+    dom.showImg(nex)
+}
+
+function prevCat(){
+    let cur = dom.currentCat(); // Current
+    let pre = slides.prevCat(cur); // Previous
+    if (pre == false){
+        return
+    }
+    dom.empty();
+    dom.showImg(pre)
+}
+
+const nexB = document.querySelector('div.next-btn');
+nexB.addEventListener('click',nextCat);
+
+const preB = document.querySelector('div.prev-btn');
+preB.addEventListener('click',prevCat);
+
+counter.fill(arr);
--- workflow.md
+++ workflow.md
@@ -4,3 +4,7 @@ set up the basic framework to start working
 - config files
 - modules
 - several outputs to draw several index.html
+
+//
+
+- link images to 
